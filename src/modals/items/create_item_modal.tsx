78d431a5285a9e1e00c12580f2bc0ff89d223bb5
Nikola Cirkovic <cirkovic.developer@gmail.com>
2023-10-29 00:10:59 +0200

    Implementation of create|delete for Items, EDIT WIP

--- src/modals/items/create_item_modal.tsx
+++ src/modals/items/create_item_modal.tsx
@@ -1,97 +1,94 @@
-import { Button, Dialog, Flex, Switch, TextField } from "@radix-ui/themes";
+import { Button, Dialog, Flex, TextField } from "@radix-ui/themes";
 import { Text } from "@radix-ui/themes";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import { ItemDTO } from "../../shared/interfaces/item.interface";
 
 interface CreateClientProps {
     cancelFunction: () => void,
     saveFunction: (form: ItemDTO) => void,
     isOpen: boolean,
-    modalType:string
+    modalType: "service" | "product"
 }
 
 
-const CreateEmployeeModal = (props: CreateClientProps) => {
+const CreateItemModal = (props: CreateClientProps) => {
     const [form, setForm] = useState<ItemDTO>({
         id: "",
         name: "",
         price: "",
         selling_price: "",
         color: "",
-        type: "service",
+        type: props.modalType,
         duration: "60",
         note: "",
     });
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        props.saveFunction(form)
+    }
 
     return (<>
         <Dialog.Root open={props.isOpen} >
             <Dialog.Content style={{ maxWidth: 450 }}>
                 <Dialog.Title> {`Create ${props.modalType}`}</Dialog.Title>
                 <Dialog.Description size="2" mb="4">
-                {`Create ${props.modalType}`}
+                    {`Create ${props.modalType}`}
                 </Dialog.Description>
 
-                <form action="">
+                <form onSubmit={handleSubmit}>
                     <Flex direction="column" gap="3">
                         <label>
                             <Text as="div" size="2" mb="1" weight="bold">
                                 Name
                             </Text>
                             <TextField.Input
+                                required={true}
                                 onChange={(e) => setForm((c) => c && { ...c, name: e.target.value })}
                                 value={form.name}
-
                             />
                         </label>
                     </Flex>
-                    <Flex direction="column" gap="3">
+                    <Flex direction="row" gap="3">
                         <label>
                             <Text as="div" size="2" mb="1" weight="bold">
                                 Price
                             </Text>
                             <TextField.Input
+                                type="number"
+                                required={true}
                                 onChange={(e) => setForm((c) => c && { ...c, price: e.target.value })}
                                 value={form.price}
 
                             />
                         </label>
-                    </Flex>
-
-                    <Flex direction="row" gap="3">
-
                         <label>
                             <Text as="div" size="2" mb="1" weight="bold">
-                                Gender
-                            </Text>
-                            <select name="gender"
-                                className="w-full rounded px-3 py-2 focus:outline-none "
-                                onChange={(e) => setForm((c) => c && { ...c, gender: e.target.value })}
-                                value={form.gender} id="gender">
-                                <option value="male">Male</option>
-                                <option value="female">Female</option>
-                            </select>
-                        </label>
-                        <label>
-                            <Text as="div" size="2" mb="1" weight="bold">
-                                Enable login
-                            </Text>
-                            <Switch
-
-                                checked={form?.login_enabled}
-                                onCheckedChange={(checked) => setForm((c) => c && ({ ...c, login_enabled: checked }))}
-                            />
-                        </label>
-                    </Flex>
-                    <Flex direction="column" gap="3">
-                        <label>
-                            <Text as="div" size="2" mb="1" weight="bold">
-                                Email
+                                Selling Price
                             </Text>
                             <TextField.Input
-                                required={true}
-                                value={form.email}
-                                onChange={(e) => setForm((c) => c && { ...c, email: e.target.value })}
+                                onChange={(e) => setForm((c) => c && { ...c, selling_price: e.target.value })}
+                                value={form.selling_price}
+
                             />
+                        </label>
+                    </Flex>
+
+
+                    <Flex direction="row" gap="3">
+                        <label>
+                            <Text as="div" size="2" mb="1" weight="bold">
+                                Duration
+                            </Text>
+                            <TextField.Input
+                                onChange={(e) => setForm((c) => c && { ...c, duration: e.target.value })}
+                                value={form.duration}
+                            />
+                        </label>
+                        <label>
+                            <Text as="div" size="2" mb="1" weight="bold">
+                                Color
+                            </Text>
+                            <input type="color" name="color" value={form.color} onChange={(e) => { setForm((c) => c && { ...c, color: e.target.value }) }} />
                         </label>
                     </Flex>
 
@@ -115,9 +112,7 @@ const CreateEmployeeModal = (props: CreateClientProps) => {
                                 Cancel
                             </Button>
                         </Dialog.Close>
-                        <Dialog.Close>
-                            <Button type="button" onClick={() => { props.saveFunction(form) }}>Save</Button>
-                        </Dialog.Close>
+                        <Button type="submit" >Save</Button>
                     </Flex>
                 </form>
             </Dialog.Content>
@@ -125,4 +120,4 @@ const CreateEmployeeModal = (props: CreateClientProps) => {
     </>)
 }
 
-export default CreateEmployeeModal
+export default CreateItemModal
