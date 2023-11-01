c94a6c88bc093ee779dc12d6a3f9e7178a9e9202
Nikola Cirkovic <cirkovic.developer@gmail.com>
2023-11-01 23:42:33 +0100

    .

--- src/modals/employees/create_employee_modal.tsx
+++ src/modals/employees/create_employee_modal.tsx
@@ -5,7 +5,7 @@ import { EmployeeDTO } from "../../shared/interfaces/employees.interface";
 
 interface CreateClientProps {
     cancelFunction: () => void,
-    saveFunction: (form:EmployeeDTO) => void,
+    saveFunction: (form: EmployeeDTO) => void,
     isOpen: boolean
 }
 
@@ -13,13 +13,13 @@ interface CreateClientProps {
 const CreateEmployeeModal = (props: CreateClientProps) => {
 
     const [form, setForm] = useState<EmployeeDTO>({
-        id:"",
-        name:"",
-        email:"",
-        gender:"male",
-        note:"",
-        job_description:"",
-        login_enabled:false
+        id: "",
+        name: "",
+        email: "",
+        gender: "male",
+        note: "",
+        job_description: "",
+        login_enabled: false
     });
 
     return (<>
@@ -30,94 +30,94 @@ const CreateEmployeeModal = (props: CreateClientProps) => {
                     Create employee
                 </Dialog.Description>
 
-             <form action="">
-             <Flex direction="column" gap="3">
-                    <label>
-                        <Text as="div" size="2" mb="1" weight="bold">
-                            Full name
-                        </Text>
-                        <TextField.Input
-                            onChange={(e) => setForm((c) => c && { ...c, name: e.target.value })}
-                            value={form.name}
+                <form action="">
+                    <Flex direction="column" gap="3">
+                        <label>
+                            <Text as="div" size="2" mb="1" weight="bold">
+                                Full name
+                            </Text>
+                            <TextField.Input
+                                onChange={(e) => setForm((c) => c && { ...c, name: e.target.value })}
+                                value={form.name}
 
-                        />
-                    </label>
-                </Flex>
-                <Flex direction="column" gap="3">
-                    <label>
-                        <Text as="div" size="2" mb="1" weight="bold">
-                            Job description
-                        </Text>
-                        <TextField.Input
-                            onChange={(e) => setForm((c) => c && { ...c, job_description: e.target.value })}
-                            value={form.job_description}
-
-                        />
-                    </label>
-                </Flex>
-
-                <Flex direction="row" gap="3">
-
-                    <label>
-                        <Text as="div" size="2" mb="1" weight="bold">
-                            Gender
-                        </Text>
-                        <select name="gender"
-                            className="w-full rounded px-3 py-2 focus:outline-none "
-                            onChange={(e) => setForm((c) => c && { ...c, gender: e.target.value })}
-                            value={form.gender} id="gender">
-                            <option value="male">Male</option>
-                            <option value="female">Female</option>
-                        </select>
-                    </label>
-                    <label>
-                        <Text as="div" size="2" mb="1" weight="bold">
-                            Enable login
-                        </Text>
-                        <Switch
-                           
-                                checked={form?.login_enabled}
-                                onCheckedChange={(checked) => setForm((c) => c && ({ ...c, login_enabled:checked }))}
                             />
-                    </label>
-                </Flex>
-                <Flex direction="column" gap="3">
-                    <label>
-                        <Text as="div" size="2" mb="1" weight="bold">
-                            Email
-                        </Text>
-                        <TextField.Input
-                        required={true}
-                            value={form.email}
-                            onChange={(e) => setForm((c) => c && { ...c, email: e.target.value })}
-                        />
-                    </label>
-                </Flex>
-              
-                <Flex direction="column" gap="3">
-                    <label>
-                        <Text as="div" size="2" mb="1" weight="bold">
-                            Note
-                        </Text>
-                        <TextField.Input
-                            value={form.note}
-                            onChange={(e) => setForm((c) => c && { ...c, note:e.target.value })}
-                        />
-                    </label>
-                </Flex>
+                        </label>
+                    </Flex>
+                    <Flex direction="column" gap="3">
+                        <label>
+                            <Text as="div" size="2" mb="1" weight="bold">
+                                Job description
+                            </Text>
+                            <TextField.Input
+                                onChange={(e) => setForm((c) => c && { ...c, job_description: e.target.value })}
+                                value={form.job_description}
 
-         
+                            />
+                        </label>
+                    </Flex>
 
-                <Flex gap="3" mt="4" justify="end">
-                    <Dialog.Close>
-                        <Button onClick={props.cancelFunction} variant="soft" color="gray">
-                            Cancel
-                        </Button>
-                    </Dialog.Close>
-                    <Dialog.Close>
-                        <Button type="button" onClick={() => {props.saveFunction(form) }}>Save</Button>
-                    </Dialog.Close>
-                </Flex>
+                    <Flex direction="row" gap="3">
+
+                        <label>
+                            <Text as="div" size="2" mb="1" weight="bold">
+                                Gender
+                            </Text>
+                            <select name="gender"
+                                className="w-full rounded px-3 py-2 focus:outline-none "
+                                onChange={(e) => setForm((c) => c && { ...c, gender: e.target.value })}
+                                value={form.gender} id="gender">
+                                <option value="male">Male</option>
+                                <option value="female">Female</option>
+                            </select>
+                        </label>
+                        <label>
+                            <Text as="div" size="2" mb="1" weight="bold">
+                                Enable login
+                            </Text>
+                            <Switch
+
+                                checked={form?.login_enabled}
+                                onCheckedChange={(checked) => setForm((c) => c && ({ ...c, login_enabled: checked }))}
+                            />
+                        </label>
+                    </Flex>
+                    <Flex direction="column" gap="3">
+                        <label>
+                            <Text as="div" size="2" mb="1" weight="bold">
+                                Email
+                            </Text>
+                            <TextField.Input
+                                required={true}
+                                value={form.email}
+                                onChange={(e) => setForm((c) => c && { ...c, email: e.target.value })}
+                            />
+                        </label>
+                    </Flex>
+
+                    <Flex direction="column" gap="3">
+                        <label>
+                            <Text as="div" size="2" mb="1" weight="bold">
+                                Note
+                            </Text>
+                            <TextField.Input
+                                value={form.note}
+                                onChange={(e) => setForm((c) => c && { ...c, note: e.target.value })}
+                            />
+                        </label>
+                    </Flex>
+
+
+
+                    <Flex gap="3" mt="4" justify="end">
+                        <Dialog.Close>
+                            <Button onClick={props.cancelFunction} variant="soft" color="gray">
+                                Cancel
+                            </Button>
+                        </Dialog.Close>
+                        <Dialog.Close>
+                            <Button type="button" onClick={() => { props.saveFunction(form) }}>Save</Button>
+                        </Dialog.Close>
+                    </Flex>
                 </form>
             </Dialog.Content>
         </Dialog.Root >
