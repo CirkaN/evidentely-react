98c439497b9364c81e3bd637e73e2ce2b34fad78
Nikola Cirkovic <cirkovic.developer@gmail.com>
2023-11-11 23:48:46 +0100

    Improve infobox so we can pass font size aswell

--- src/components/info-box.tsx
+++ src/components/info-box.tsx
@@ -3,6 +3,7 @@ interface infoProps {
     headerText: string,
     text: string,
     type: InfoBoxType,
+    fontSize?: string,
 }
 // eslint-disable-next-line react-refresh/only-export-components
 export enum InfoBoxType {
@@ -13,26 +14,27 @@ export enum InfoBoxType {
 
 const InfoBox = (props: infoProps) => {
 
-    if(props.type == 'info'){
+    if (props.type == 'info') {
         return (
-        <div className="bg-blue-100 border-l-4 border-blue-500 text-blue-700 p-4" role="alert">
-        <span className="font-bold">{props.headerText}</span> <br />
-        <span>{props.text}.</span>
-    </div>);
-    }else if(props.type == 'danger'){
+            <div className="bg-blue-100 border-l-4 border-blue-500 text-blue-700 p-4" role="alert">
+                <span className="font-bold">{props.headerText}</span> <br />
+                <span className={props.fontSize ?? 'text-md'}>{props.text}.</span>
+            </div>);
+    } else if (props.type == 'danger') {
         return (<div className="bg-red-100 border-l-4 border-red-500 text-red-700 p-4" role="alert">
-        <p className="font-bold">{props.headerText}</p>
-        <p>{props.text}.</p>
-    </div>);
-    }else{
+            <span className="font-bold">{props.headerText}</span> <br />
+            <span className={props.fontSize ?? 'text-md'}>{props.text}.</span>
+
+        </div>);
+    } else {
         return (<div className="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4" role="alert">
-        <p className="font-bold">{props.headerText}</p>
-        <p>{props.text}.</p>
-    </div>);
+            <span className="font-bold">{props.headerText}</span> <br />
+            <span className={props.fontSize ?? 'text-md'}>{props.text}.</span>
+        </div>);
     }
 
 
-    
+
 }
 
 export default InfoBox;
