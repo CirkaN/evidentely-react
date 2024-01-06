8ddeef8cfadc5fa4bb842acd4127d77fbf90dcd9
Nikola Cirkovic <cirkovic.developer@gmail.com>
2024-01-07 00:39:11 +0100

    Fixed icons for login page

--- src/routes/login/login.tsx
+++ src/routes/login/login.tsx
@@ -38,7 +38,7 @@ const Login = () => {
         'name': response.data.user.name,
         'avatar_url': response.data.user.avatar_url,
         'company_name': response.data.user.company_name,
-        'business_type_slug':response.data.user.business_type_slug,
+        'business_type_slug': response.data.user.business_type_slug,
       })
     }).catch(() => {
       setHasErrors(true);
@@ -121,12 +121,6 @@ const Login = () => {
 
                   <button
                     className="mt-5 tracking-wide font-semibold bg-indigo-500 text-gray-100 w-full py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none">
-                    <svg className="w-6 h-6 -ml-2" fill="none" stroke="currentColor" strokeWidth="2"
-                      strokeLinecap="round" strokeLinejoin="round">
-                      <path d="M16 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2" />
-                      <circle cx="8.5" cy="7" r="4" />
-                      <path d="M20 8v6M23 11h-6" />
-                    </svg>
                     <span className="ml-3">
                       Uloguj se
                     </span>
@@ -134,7 +128,12 @@ const Login = () => {
                   <button
                     onClick={() => { openRegistration() }}
                     className="mt-5 tracking-wide font-semibold bg-blue-500 text-gray-100 w-full py-2 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none">
-
+                    <svg className="w-6 h-6 -ml-2" fill="none" stroke="currentColor" strokeWidth="2"
+                      strokeLinecap="round" strokeLinejoin="round">
+                      <path d="M16 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2" />
+                      <circle cx="8.5" cy="7" r="4" />
+                      <path d="M20 8v6M23 11h-6" />
+                    </svg>
                     <span className="ml-3">
                       Nemate nalog? Registrujte se
                     </span>
