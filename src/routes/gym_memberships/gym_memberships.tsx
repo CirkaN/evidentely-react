2b640b4cc3e12f69995bf7cd71443760c56665aa
Nikola Cirkovic <cirkovic.developer@gmail.com>
2024-01-07 00:33:44 +0100

    Set membership inactive logic, updated localization

--- src/routes/gym_memberships/gym_memberships.tsx
+++ src/routes/gym_memberships/gym_memberships.tsx
@@ -1,14 +1,26 @@
 import { t } from "i18next"
 import DataTable, { Action, ActionTypes, Field, TableAction } from "../../components/datatable"
 import { GymMembershipDTO } from "../../shared/interfaces/gym_memberships.interface"
-import { Plus, Trash } from "react-feather"
+import { AlertTriangle, Check, Plus, Trash } from "react-feather"
 import { useState } from "react"
 import CreateGymMembershipModal from "../../modals/gym_memberships/create_gym_membership_modal"
+import SweetAlert2 from "react-sweetalert2"
+import axios_instance from "../../config/api_defaults"
+import toast from "react-hot-toast"
+import { useQueryClient } from "react-query"
 
 const GymMemberships = () => {
     const [createMembershipModalOpen, setCreateMembershipModalOpen] = useState(false);
-    
+    const [swalProps, setSwalProps] = useState({});
+    const queryClient = useQueryClient();
     const fields: Field[] = [
+        {
+            name: t('common.client'),
+            original_name: "member_name",
+            show: true,
+            editable_from_table: false,
+            has_sort: true
+        },
         {
             name: "Plan",
             original_name: "membership_plan_name",
@@ -31,8 +43,9 @@ const GymMemberships = () => {
             has_sort: true
         },
         {
-            name: "Status",
+            name: t('sales.status'),
             original_name: "status",
+            formatFn: (status) => formatStatus(status),
             show: true,
             editable_from_table: false,
             has_sort: true
@@ -45,11 +58,20 @@ const GymMemberships = () => {
             has_sort: true
         }
     ]
+    const formatStatus = (status: string) => {
+        if (status === 'active') {
+            return <Check color="green" />
+        }
+        if (status === 'expired') {
+            return <AlertTriangle color="red" />
+        }
+        return <AlertTriangle color="red" />
+    }
     const actions: Action<GymMembershipDTO>[] = [
         {
             type: ActionTypes.Delete,
             icon: <Trash color="red"></Trash>,
-            fn: (gymMembership: GymMembershipDTO) => { alert(gymMembership.id) }
+            fn: (gymMembership: GymMembershipDTO) => { raiseDeleteAlert(gymMembership.id) }
         },
     ]
     const table_actions: TableAction[] = [
@@ -58,13 +80,41 @@ const GymMemberships = () => {
             fn: () => { setCreateMembershipModalOpen(true) }
         }
     ];
+    const raiseDeleteAlert = (id: string) => {
+        setSwalProps({
+            show: true,
+            icon: 'error',
+            title: t('common.please_confirm'),
+            text: t('gym_memberships.delete_membership_message'),
+            cancelButtonColor: "green",
+            reverseButtons: true,
+            showCancelButton: true,
+            showConfirmButton: true,
+            cancelButtonText: t('common.cancel'),
+            confirmButtonText: t('gym_memberships.confirm_button_delete'),
+            confirmButtonColor: "red",
+            onConfirm: () => { deleteMembership(id) },
+            onResolve: setSwalOff
+        });
+    }
+    const deleteMembership = (id: string) => {
+        axios_instance().post(`/gym_memberships/${id}/set_inactive`).then(() => {
+            toast.success(t('gym_memberships.success_delete'))
+            queryClient.invalidateQueries(['gym_memberships'])
+        })
 
+    }
+    function setSwalOff() {
+        const dataCopied = JSON.parse(JSON.stringify(swalProps));
+        dataCopied.show = false;
+        setSwalProps(dataCopied);
+    }
     return (
         <>
-        
+            <SweetAlert2 {...swalProps} />
             <CreateGymMembershipModal
-            isOpen={createMembershipModalOpen}
-            closeModalFunction={()=>setCreateMembershipModalOpen(false)}
+                isOpen={createMembershipModalOpen}
+                closeModalFunction={() => setCreateMembershipModalOpen(false)}
             />
 
             <DataTable
