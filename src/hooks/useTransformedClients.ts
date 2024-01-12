017145a2f9362876e80be69c9854b60e802935d3
Nikola Cirkovic <cirkovic.developer@gmail.com>
2024-01-12 12:55:33 +0100

    Custom hooks for transformed clients and services

---
+++ src/hooks/useTransformedClients.ts
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import { TransformedDataForSelect } from '../shared/interfaces/select_box.interface';
+import { ClientDTO } from '../shared/interfaces/client.interface';
+import axios_instance from '../config/api_defaults';
+import { useQuery } from 'react-query';
+
+const useTransformedClients = () => {
+
+    const [clientTransformedList, setClientTransformedList] = useState<TransformedDataForSelect[]>();
+    const [clientList, setClientList] = useState<ClientDTO[]>()
+
+    useQuery({
+        queryKey: ['fetch_clients'],
+        retryOnMount: false,
+        queryFn: () => {
+            axios_instance().get('/clients').then(r => {
+                const transformed = r.data.map((element: ClientDTO) => ({
+                    value: element.id,
+                    label: element.name
+                }));
+                setClientTransformedList(transformed)
+                setClientList(r.data);
+            })
+        }
+    })
+   
+
+    return { clientTransformedList, clientList };
+};
+
+export default useTransformedClients;
