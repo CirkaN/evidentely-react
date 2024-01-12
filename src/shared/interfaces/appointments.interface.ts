dea85f79fcfebd6faf953018efb84bb93138f48d
Nikola Cirkovic <cirkovic.developer@gmail.com>
2024-01-12 12:54:42 +0100

    Refactor of appointment types

--- src/shared/interfaces/appointments.interface.ts
+++ src/shared/interfaces/appointments.interface.ts
@@ -1,36 +1,31 @@
 interface Appointment {
-    user_id:string,
-    price: string,
-    amount_paid?:string,
-    due_amount:string,
-    paid_at?:string,
-    employee_id?:string,
-    title:string,
+    user_id: string,
+    price: string|number,
+    amount_paid?: string,
+    due_amount: string,
+    paid_at?: string,
+    employee_id?: string,
+    title: string,
     start: string,
     end: string,
-    color?:string,
+    color?: string,
     remind_client: boolean,
     item_id?: string,
-    status:string,
-    end_formatted?:string,
-    start_formatted?:string
-    note?:string
-    remind_setting: AppointmentRemindSettings
+    status: string,
+    end_formatted?: string,
+    start_formatted?: string
+    note?: string
+    remind_setting: {
+        remind_day_before: boolean,
+        remind_same_day: boolean,
+        remind_now: boolean,
+        remind_for_upcoming: boolean,
+        settings_for_upcoming: {
+            date: string,
+            custom_text_message: string
+        }
+    }
 }
 
-interface AppointmentRemindSettingsType {
-    remind_day_before: boolean,
-    remind_same_day: boolean,
-    remind_now: boolean,
-    remind_for_upcoming: boolean,
-    settings_for_upcoming: AppointmentUpcomingSettings
-}
-interface AppointmentUpcomingSettings {
-    date: string,
-    custom_text_message: string
-}
-
-
-export type AppointmentType = Appointment;
-export type AppointmentRemindSettings = AppointmentRemindSettingsType;
+export type AppointmentDTO = Appointment;
 
