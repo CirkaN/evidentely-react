3ea29b10d13ddaad52cbf4144fb8090e7ef6d55c
Nikola Cirkovic <cirkovic.developer@gmail.com>
2024-02-01 17:59:36 +0100

    Add description field to items

--- src/shared/interfaces/item.interface.ts
+++ src/shared/interfaces/item.interface.ts
@@ -2,6 +2,7 @@ export type ItemDTO = {
     id: string;
     type: "product" | "service" | "package";
     name: string;
+    description?: string;
     selling_price: string;
     price: string;
     duration: string;
