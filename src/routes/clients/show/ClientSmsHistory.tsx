278683c9942ba7d4f6b0435d912bfeeedb5b1ab9
Nikola Cirkovic <cirkovic.developer@gmail.com>
2024-02-02 17:27:52 +0100

    Show whole sms log text in the modal

--- src/routes/clients/show/ClientSmsHistory.tsx
+++ src/routes/clients/show/ClientSmsHistory.tsx
@@ -1,10 +1,37 @@
 import { useParams } from "react-router-dom";
 import ClientDetailsHeader from "../../../layouts/clients/details_header";
-import DataTable, { Field } from "../../../components/datatable";
+import DataTable, {
+    Action,
+    ActionTypes,
+    Field,
+} from "../../../components/datatable";
+import { Eye } from "react-feather";
+import { useState } from "react";
+import ShowSmsLogModal from "../../../modals/sms_logs/show_sms_log";
+
+interface SmsLogDTO {
+    id: number;
+    text_message: string;
+    appointment_name: string;
+}
 
 const ClientSmsHistory = () => {
     const { id } = useParams();
     const url = `user/${id}/sms_logs?per_page=5`;
+    const [isShowModalOpen, setIsShowModalOpen] = useState(false);
+    const [smsLogText, setSmsLogText] = useState("");
+
+    const fieldActions: Action<SmsLogDTO>[] = [
+        {
+            type: ActionTypes.Show,
+            icon: <Eye color="green" />,
+            fn: (smsLog: SmsLogDTO) => {
+                setIsShowModalOpen(true);
+                setSmsLogText(smsLog.text_message);
+            },
+        },
+    ];
+
     const fields: Field[] = [
         {
             name: "Poruka",
@@ -40,9 +67,17 @@ const ClientSmsHistory = () => {
         <>
             <div className="h-screen w-full p-10  pt-10 sm:pt-0">
                 {id && <ClientDetailsHeader id={id} />}
+                <ShowSmsLogModal
+                    isOpen={isShowModalOpen}
+                    smsLog={smsLogText}
+                    closeModal={() => {
+                        setIsShowModalOpen(false);
+                    }}
+                />
                 <DataTable
-                    has_actions={false}
+                    has_actions={true}
                     fields={fields}
+                    actions={fieldActions}
                     url={url}
                     has_search={false}
                     table_name={"Istorija poruka"}
